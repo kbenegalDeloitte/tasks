c6b0e34de3c15b0da830d6e1a28f028c4ff7603f
EC2 Default User <ec2-user@ip-10-1-0-179.ec2.internal>
2022-09-09 17:05:21 +0000

    added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "tasks6cc8164d": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, Authenticator } from '@aws-amplify/ui-react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2...this is tasks</h1>
+        <h1>Hello from V2</h1>
       </header>
+      <Authenticator>
+  		  {({ signOut, user }) => (
+  			<main>
+  			  <h1>Hello {user.username}</h1>
+  			  <button onClick={signOut}>Sign out</button>
+  			</main>
+  		  )} 
+  		</Authenticator>
     </div>
+    
   );
 }
 
